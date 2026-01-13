5caa72dea2db4c67cfe130e811aa7927ec04b3fa
Carlos Smith <csmithtay@gmail.com>
2026-01-13 14:08:19 -0500

    select-fruta-pk

--- frontend/js/apicall.js
+++ frontend/js/apicall.js
@@ -2,31 +2,46 @@ async function getDataTuneles(started_from, started_to, temporada_anio, limit, o
 
     const url = `http://127.0.0.1:8000/procesos?temporada_anio=${temporada_anio}&limit=${limit}&offset=${offset}`;
     try{
-        const response = await fetch(url)
+        const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Error en la solicitud');
-        
-    }
+        }
 
-    const result = await response.json();
-    console.log(result);
-} catch (error) {
-    console.error('Error:', error);
-}
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 } 
 
 async function getTemporadas(){
     const url = `http://127.0.0.1:8000/temporadas`;
     try{
-        const response = await fetch(url)
+        const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Error en la solicitud');
         }
         const result = await response.json();
-        console.log(result);
+        return result;
     }
     catch (error) {
         console.error('Error:', error);
+        return [];
     }
-    return result;
+}
+
+async function getPackings(temporada, fruta){
+    const url = `http://127.0.0.1:8000/packings?temporada=${temporada}&fruta=${fruta}`;
+    try{
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Error en la solicitud');
+        }
+        const result = await response.json();
+        return result;
+    }
+    catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
 } 
